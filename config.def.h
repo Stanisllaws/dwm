a56749d053d92a431274211c3646aa64ca5e9461
rafal <you@example.com>
2026-02-12 11:13:31 +0100

    Add web-selector

--- config.def.h
+++ config.def.h
@@ -62,17 +62,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 
-static const char *chromiumcmd[] = { "chromium",NULL };
-static const char *bravecmd[] = { "brave",NULL };
-static const char *firefoxcmd[] = { "firefox",NULL };
+static const char *webcmd[] = { "web-select",NULL };
 static const char *termcmd[]  = { "st", NULL };
 //static const char *alacrittycmd[]  = { "alacritty", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	
-        { MODKEY|ShiftMask,             XK_w,          spawn,          {.v = firefoxcmd } },
-	{ MODKEY|Mod1Mask,              XK_w,          spawn,          {.v = chromiumcmd } },
-	{ MODKEY,                       XK_w,          spawn,          {.v = bravecmd } },	
+	{ MODKEY,                       XK_w,          spawn,          {.v = webcmd } },	
 	{ MODKEY,                       XK_space,      spawn,          {.v = dmenucmd } },
 	//{ MODKEY,                       XK_Return,      spawn,          {.v = alacrittycmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
